ca2b68049745477485b4631f1ad620d1bb9d60b6
SKINKA <oberon4589@gmail.com>
2023-12-05 14:10:39 -0300

    style: aplicando estilizações

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const image = document.querySelector('img');
 input = document.querySelector('input');
 const button = document.querySelector('button');
-api = 'https://api.qrserver.com/v1/',
-api2 = 'create-qr-code/?size=150x150&data=';
+(api = 'https://api.qrserver.com/v1/'),
+    (api2 = 'create-qr-code/?size=150x150&data=');
 
 button.addEventListener('click', () => {
     image.src = `${api}${api2}${input.value}`;
